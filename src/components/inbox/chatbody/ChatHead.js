dc6ccecfa6b7b927106e97475d7f87852f502c44
mamyh <likhmaon@gmail.com>
2022-09-21 06:35:17 +0600

    module 9 completed

--- src/components/inbox/chatbody/ChatHead.js
+++ src/components/inbox/chatbody/ChatHead.js
@@ -1,12 +1,22 @@
-export default function ChatHead({ avatar, name }) {
+import getAvatar from "gravatar-url";
+import { useSelector } from "react-redux";
+
+export default function ChatHead({ message}) {
+    const {user} = useSelector(state=>state.auth)||{};
+    const {email} = user ||{};
+    const {sender,reciver} = message ||{}
+    const partnar = sender.email === email ?reciver : sender;
+
     return (
         <div className="relative flex items-center p-3 border-b border-gray-300">
             <img
                 className="object-cover w-10 h-10 rounded-full"
-                src={avatar}
-                alt={name}
+                src={getAvatar(partnar.email,{
+                    size:80
+                })}
+                alt={partnar.name}
             />
-            <span className="block ml-2 font-bold text-gray-600">{name}</span>
+            <span className="block ml-2 font-bold text-gray-600">{partnar.name}</span>
         </div>
     );
 }
--- src/components/inbox/chatbody/Messages.js
+++ src/components/inbox/chatbody/Messages.js
@@ -4,10 +4,12 @@ import Message from "./Message";
 export default function Messages({messages=[]}) {
     const {user} = useSelector(state=> state.auth) ||{};
     const {email} = user ||{}
+
+    
     return (
         <div className="relative w-full h-[calc(100vh_-_197px)] p-6 overflow-y-auto flex flex-col-reverse">
             <ul className="space-y-2">
-                {messages.map(message=>{
+                {messages.slice().sort((a,b)=> a.timestamp - b.timestamp).map(message=>{
                    const {id,sender,message:lastMessage} = message||{};
                    const justify = sender.email === email ? 'end':'start';
                    return <Message justify={justify} key={id} message={lastMessage} />
