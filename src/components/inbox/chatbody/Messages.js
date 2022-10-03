5a4a0250ee4068eac3bd156868014c529f4a41e9
mamyh <likhmaon@gmail.com>
2022-10-03 23:19:48 +0600

    fixed top scrolling issue

--- src/components/inbox/chatbody/Messages.js
+++ src/components/inbox/chatbody/Messages.js
@@ -5,26 +5,33 @@ import Message from "./Message";
 export default function Messages({pages =1, messages=[]}) {
     const {user} = useSelector(state=> state.auth) ||{};
     const {email} = user ||{}
-
+    
+    console.log(messages.length)
     
     return (
-        <div className="relative w-full h-[calc(100vh_-_197px)] p-6 overflow-y-auto flex flex-col-reverse">
-            <ul className="space-y-2">
-                <InfiniteScroll
+        <div id="scrollableDiv" className="relative w-full h-[calc(100vh_-_197px)] p-6 overflow-y-auto flex flex-col-reverse">
+            <InfiniteScroll
                     dataLength={messages?.length}
                     next={()=>console.log('fetching',pages)}
-                    hasMore={false}
+                    hasMore={true}
+                    height={window.innerHeight-197}
+                    inverse={true}
+                    style={{display:"flex",flexDirection:"column-reverse"}}
+                    scrollableTarget="scrollableDiv"
                 >
-                    {messages.slice().sort((a,b)=> a.timestamp - b.timestamp).map(message=>{
-                    const {sender,message:lastMessage} = message||{};
-                    const justify = sender.email === email ? 'end':'start';
-                    return <Message justify={justify} key={Math.random()} message={lastMessage} />
-                    })}
+                    <ul className="space-y-2">
+                        
+                            {messages.slice().sort((a,b)=> a.timestamp - b.timestamp).map(message=>{
+                            const {sender,message:lastMessage} = message||{};
+                            const justify = sender.email === email ? 'end':'start';
+                            return <Message justify={justify} key={Math.random()} message={lastMessage} />
+                            })}
 
-                </InfiniteScroll>
-                
-                 
-            </ul>
+                        
+                        
+                        
+                    </ul>
+            </InfiniteScroll>
         </div>
     );
 }
